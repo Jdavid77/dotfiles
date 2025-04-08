b409c930d83fb9d546796faa7386e1c443bc799c
jnobrega <jnobrega@neoception.com>
2025-04-08 15:37:56 +0000

    chore: add chezmoi to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hello"
+set -euo pipefail
+
+if ! command -v chezmoi >/dev/null; then
+    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/Jdavid77/dotfiles-demo.git
+fi
+
+exit 0
+
