1d097dd706f109e5b4355d49fa468fb23dfbc010
jnobrega <jnobrega@neoception.com>
2025-04-12 15:40:45 +0000

    chore: add alacritty info

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
+# allacrity info
+curl -sSL https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | tic -x -
+
 if command -v zsh >/dev/null; then
-    sudo chsh -s $(command -v zsh) $USER
+  sudo chsh -s $(command -v zsh) $USER
 fi
 
 if ! command -v chezmoi >/dev/null; then
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/Jdavid77/dotfiles-demo.git
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply https://github.com/Jdavid77/dotfiles-demo.git
 fi
 
 exit 0
-
