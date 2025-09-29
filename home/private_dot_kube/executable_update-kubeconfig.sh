1654b1942a513020228550ce98834def3b7d6506
joao.nobrega <joao.nobrega@definedcrowd.com>
2025-09-29 22:52:08 +0100

    chore: add work config in kube script

--- home/private_dot_kube/executable_update-kubeconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-mkdir -p ~/.kube/backup
-DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-echo $DATE
-mv config backup/config-$DATE
-# kubeconfigs separated by ":"
-KUBECONFIG=<kubeconfigs-here> kubectl config view --merge --flatten >config
